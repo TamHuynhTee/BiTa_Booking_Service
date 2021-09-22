0dce972bdc832686a8de1585fe7e014e27f15fc0
Huynh Thanh Tam <huynhthanhtam2605@gmail.com>
2021-09-22 23:25:15 +0700

    use vite instead of craete-react-app

---
+++ Frontend/src/views/home/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './style.scss';
+
+interface HomePageProps {}
+
+export const HomePage = (props: HomePageProps) => {
+    return <div></div>;
+};
---
+++ Frontend/src/views/profile/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './style.scss';
+
+interface ProfileProps {}
+
+export const Profile = (props: ProfileProps) => {
+    return <div></div>;
+};
