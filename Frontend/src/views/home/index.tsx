9e990b7d2cbf9b3dd76f8f5c85041cb9c118a13a
Huynh Thanh Tam <huynhthanhtam2605@gmail.com>
2021-09-27 20:28:06 +0700

    [feat] responsive homepage and add slider

--- Frontend/src/views/home/index.tsx
+++ Frontend/src/views/home/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Slider from 'react-slick';
 import './style.scss';
 import schedule from '../../images/online-schedule.svg';
 import fast from '../../images/fast.svg';
@@ -6,51 +7,12 @@ import simple from '../../images/simple.svg';
 import wallet from '../../images/wallet.svg';
 import user from '../../images/user.svg';
 import business from '../../images/business.svg';
+import booked from '../../images/booked.svg';
+import plan from '../../images/plan.svg';
 import { LoginDialog } from '../../components/login';
 import { ForgotPassDialog } from '../../components/forgotPassword';
 
 interface HomePageProps {}
-
-const ChooseAccountDialog = () => {
-    return (
-        <div className="modal fade" id="ChooseAccountModal" aria-hidden="true">
-            <div className="modal-dialog modal-dialog-centered modal-xl">
-                <div className="modal-content">
-                    <div className="modal-header">
-                        <h5 className="modal-title" id="exampleModalLabel">
-                            Chọn tài khoản
-                        </h5>
-                        <button
-                            type="button"
-                            className="btn-close"
-                            data-bs-dismiss="modal"
-                            aria-label="Close"
-                        ></button>
-                    </div>
-                    <div className="modal-body">
-                        <h1 style={{ textAlign: 'center', color: '#6c63ff' }}>
-                            Bạn muốn trở thành?
-                        </h1>
-                        <div className="chooseAccount">
-                            <div className="chooseAccount-card">
-                                <h2>Khách hàng</h2>
-                                <div className="chooseAccount-card-image">
-                                    <img src={user} alt="" />
-                                </div>
-                            </div>
-                            <div className="chooseAccount-card">
-                                <h2>Đối tác dịch vụ</h2>
-                                <div className="chooseAccount-card-image">
-                                    <img src={business} alt="" />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-};
 
 export const HomePage = (props: HomePageProps) => {
     return (
@@ -70,9 +32,7 @@ export const HomePage = (props: HomePageProps) => {
                         Bắt đầu ngay
                     </button>
                 </div>
-                <div className="homepage-banner-image">
-                    <img src={schedule} alt="" />
-                </div>
+                <BannerSlider />
             </div>
             <div className="container">
                 <div className="row homepage-utilities">
@@ -111,6 +71,73 @@ export const HomePage = (props: HomePageProps) => {
             <LoginDialog />
             <ForgotPassDialog />
             <ChooseAccountDialog />
+        </div>
+    );
+};
+
+const BannerSlider = () => {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        // autoplay: true,
+    };
+    return (
+        <div className="homepage-banner-image">
+            <Slider {...settings}>
+                <div className="homepage-banner-image-container">
+                    <img src={schedule} alt="" />
+                </div>
+                <div className="homepage-banner-image-container">
+                    <img src={booked} alt="" />
+                </div>
+                <div className="homepage-banner-image-container">
+                    <img src={plan} alt="" />
+                </div>
+            </Slider>
+        </div>
+    );
+};
+
+const ChooseAccountDialog = () => {
+    return (
+        <div className="modal fade" id="ChooseAccountModal" aria-hidden="true">
+            <div className="modal-dialog modal-dialog-centered modal-xl">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title" id="exampleModalLabel">
+                            Chọn tài khoản
+                        </h5>
+                        <button
+                            type="button"
+                            className="btn-close"
+                            data-bs-dismiss="modal"
+                            aria-label="Close"
+                        ></button>
+                    </div>
+                    <div className="modal-body">
+                        <h1 style={{ textAlign: 'center', color: '#6c63ff' }}>
+                            Bạn muốn trở thành
+                        </h1>
+                        <div className="chooseAccount">
+                            <div className="chooseAccount-card">
+                                <h2>Khách hàng</h2>
+                                <div className="chooseAccount-card-image">
+                                    <img src={user} alt="" />
+                                </div>
+                            </div>
+                            <div className="chooseAccount-card">
+                                <h2>Đối tác dịch vụ</h2>
+                                <div className="chooseAccount-card-image">
+                                    <img src={business} alt="" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
