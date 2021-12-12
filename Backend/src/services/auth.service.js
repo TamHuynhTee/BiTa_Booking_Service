14152999da87e13891bd90e782719a2e1dec68f9
Huynh Thanh Tam <huynhthanhtam2605@gmail.com>
2021-12-13 00:45:19 +0700

    [fix] verify email BE

--- Backend/src/services/auth.service.js
+++ Backend/src/services/auth.service.js
@@ -3,7 +3,7 @@ const tokenService = require('./token.service');
 const userService = require('./user.service');
 const businessService = require('./business.service');
 const emailService = require('./email.service');
-const Token = require('../models/token.model');
+const { Token } = require('../models');
 const ApiError = require('../utils/ApiError');
 const { tokenTypes } = require('../config/tokens');
 
