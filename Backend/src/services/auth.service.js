93583cf70fac7afc9b7515fdc0ed26c7ac287644
Huynh Thanh Tam <huynhthanhtam2605@gmail.com>
2021-12-13 00:33:00 +0700

    [test] host BE

--- Backend/src/services/auth.service.js
+++ Backend/src/services/auth.service.js
@@ -76,7 +76,7 @@ const verifyEmail = async (verifyEmailToken) => {
     await userService.updateUserById(user.id, { isEmailVerified: true });
     await Token.deleteMany({ user: user.id, type: tokenTypes.VERIFY_EMAIL });
   } catch (error) {
-    throw new ApiError(httpStatus.UNAUTHORIZED, 'Email verification failed');
+    throw new ApiError(httpStatus.UNAUTHORIZED, 'Email verification failed' + error);
   }
 };
 
